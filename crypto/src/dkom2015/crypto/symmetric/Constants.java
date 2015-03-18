e86fabd9eef7bedb33b82cf96f68118e7e892838
Vasil Panushev <vasil.panushev@sap.com>
2015-03-18 15:16:37 +0200

    add symmetric encryptin samples

---
+++ crypto/src/dkom2015/crypto/symmetric/Constants.java
@@ -0,0 +1,6 @@
+package dkom2015.crypto.symmetric;
+
+public class Constants {
+  public static final String TO_ENCRYPT = "test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test";
+
+}
